43dce29b773b4318b0581137af86b99c085663b6
abhinesh653 <abhinesh653@gmail.com>
2026-03-10 21:36:21 +0530

    Update Employee.java

--- model/Employee.java
+++ model/Employee.java
@@ -13,4 +13,37 @@ public class Employee {
     private String email;
     private String department;
 
+    public Employee() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
