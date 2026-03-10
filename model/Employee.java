f1c4638a51d6659c2b64825c32a9a6a06acfc56a
abhinesh653 <abhinesh653@gmail.com>
2026-03-10 21:33:12 +0530

    Add Employee entity with JPA annotations

--- model/Employee.java
+++ model/Employee.java
@@ -1,1 +1,16 @@
+package com.example.ems.model;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String department;
+
+}
