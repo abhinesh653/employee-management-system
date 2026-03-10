bbd33902bb20495584bc7866c8eefdee29da827e
abhinesh653 <abhinesh653@gmail.com>
2026-03-10 21:39:17 +0530

    Update EmployeeController.java

--- controller/EmployeeController.java
+++ controller/EmployeeController.java
@@ -1,1 +1,33 @@
+package com.example.ems.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.ems.model.Employee;
+import com.example.ems.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        service.deleteEmployee(id);
+        return "Employee deleted successfully";
+    }
+}
