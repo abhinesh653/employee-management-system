1233d55f9704f9e693411e6b75ab718f7ffa92fe
abhinesh653 <abhinesh653@gmail.com>
2026-03-10 21:38:29 +0530

    Implement EmployeeService with CRUD operations

--- service/EmployeeService.java
+++ service/EmployeeService.java
@@ -1,1 +1,28 @@
+package com.example.ems.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ems.model.Employee;
+import com.example.ems.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public Employee saveEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+
+    public void deleteEmployee(Long id) {
+        repository.deleteById(id);
+    }
+}
