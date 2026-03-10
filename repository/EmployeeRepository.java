c248daa406a721302908c6296b783ba1c44bd486
abhinesh653 <abhinesh653@gmail.com>
2026-03-10 21:37:18 +0530

    Add EmployeeRepository interface for JPA

--- repository/EmployeeRepository.java
+++ repository/EmployeeRepository.java
@@ -1,1 +1,8 @@
+package com.example.ems.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.ems.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
