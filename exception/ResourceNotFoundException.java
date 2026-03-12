80f1a2ca1406d9f56998cf96ac7f817b4edd6e71
N Abhineshwar Reddy <abhinesh653@gmail.com>
2026-03-12 13:59:14 +0530

    Add ResourceNotFoundException for employee error handling

--- exception/ResourceNotFoundException.java
+++ exception/ResourceNotFoundException.java
@@ -1,1 +1,8 @@
+package com.example.ems.exception;
 
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
