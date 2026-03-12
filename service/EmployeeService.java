2a47dcb4d9d5b5c3fa7ee5491923f0231d3069d3
N Abhineshwar Reddy <abhinesh653@gmail.com>
2026-03-12 14:04:20 +0530

    Add getEmployeeById method with exception handling

--- service/EmployeeService.java
+++ service/EmployeeService.java
@@ -1,12 +1,11 @@
 package com.example.ems.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.ems.model.Employee;
 import com.example.ems.repository.EmployeeRepository;
+import com.example.ems.exception.ResourceNotFoundException;
 
 @Service
 public class EmployeeService {
@@ -20,6 +19,11 @@ public class EmployeeService {
 
     public List<Employee> getAllEmployees() {
         return repository.findAll();
+    }
+    
+    public Employee getEmployeeById(Long id) {
+    return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
     }
 
     public void deleteEmployee(Long id) {
